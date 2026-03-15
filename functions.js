0f20382214b69c6107a6b1d2464f4d85f598d290
xandytec <carvalhoxandy22@gmail.com>
2026-03-15 09:16:59 -0400

    Add someEvent function with alerts

--- functions.js
+++ functions.js
@@ -1,1 +1,7 @@
-functions.js
+// functions.js
+function someEvent() {
+    alert("hello word from Danki code!");
+    alert("this is a projec from curse English for programing");
+}
+
+// Você pode adicionar mais funções aqui depois
